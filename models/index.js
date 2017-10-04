4a02d63e7b971d9181ab9d6eb7af22cdec716a26
Blanca Sanchez <blancayllen@gmail.com>
2017-10-04 10:26:10 -0400

    synced db

--- models/index.js
+++ models/index.js
@@ -1,31 +1,39 @@
 var Sequelize = require('sequelize');
-const db = new Sequelize('postgres://localhost:5432/tripPlanner');
+const db = new Sequelize('postgres://localhost:5432/tripPlanner', { logging: false });
 
-Hotel = db.define({
+Hotel = db.define('hotel', {
   name: Sequelize.STRING,
   num_stars: Sequelize.FLOAT(1, 5),
   amenities: Sequelize.TEXT
 });
 
-Restaurant = db.define({
+Restaurant = db.define('restaurant', {
   name: Sequelize.STRING,
   cuisine: Sequelize.TEXT,
   price: Sequelize.INTEGER
 })
 
-Activity = db.define({
+Activity = db.define('activity', {
   name: Sequelize.STRING,
   age_range: Sequelize.STRING
 })
 
-Place = db.define({
+Place = db.define('place', {
   address: Sequelize.STRING,
   city: Sequelize.STRING,
   state: Sequelize.STRING,
-  phone: Sequlize.STRING,
+  phone: Sequelize.STRING,
   location: Sequelize.ARRAY(Sequelize.FLOAT)
 })
 
 Hotel.belongsTo(Place);
 Restaurant.belongsTo(Place);
 Activity.belongsTo(Place);
+
+module.exports = {
+  db: db,
+  hotel: Hotel,
+  restaurant: Restaurant,
+  activity: Activity,
+  place: Place
+}
